b3df830668b80fb663ce6ea6f6fa2bd8382904d4
Modofado <44951628+Modofado@users.noreply.github.com>
2018-11-15 18:12:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,8 +35,8 @@ var channel = "511569524353531906";
 })
 
 client.on('ready', async() => {
-var server = "499933149019766785"; 
-var channel = "499933149573283862";
+var server = "511569524353531904"; 
+var channel = "511569524353531906";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#daily')
     },86400);
