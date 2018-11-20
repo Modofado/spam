084363d6b8e18a1f97159e032131b862f399bf95
Modofado <44951628+Modofado@users.noreply.github.com>
2018-11-20 21:07:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,44 +1,17 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame('ItZMeDoPlayZMC');
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);  
 });
-client.on('ready', async() => {
-var server = "511569524353531904"; 
-var channel = "511569524353531906";
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز')
-    },305);
-})
+
 
 client.on('ready', async() => {
-var server = "511569524353531904"; 
-var channel = "511569524353531906";
+var server = "511569524353531904"; //
+var channel = "511569524353531906";//
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#daily')
-    },86400);
+    client.guilds.get(server).channels.get(channel).send('**Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam ,  , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , **')
+    },400);
 })
-client.login(process.env.BOT_TOKEN);
+
+client.login(account token);
