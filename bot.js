05d920e136587fd50d665afcabd0c2cda6c0ff29
Modofado <44951628+Modofado@users.noreply.github.com>
2018-11-20 19:06:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame('ItZMeDoPlayZMC');
+ client.user.setGame(strraming'ItZMeDoPlayZMC');
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
